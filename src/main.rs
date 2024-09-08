1f40d0e1c240b9f1e0a7d81bf54596f1ed1f0ad5
abhishekprajapati1 <ap0661236@gmail.com>
2024-09-08 12:39:09 +0530

    :fire: updated readme

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod bca;
+mod algorithms;
 fn main() {
     println!("Performing linear search.");
-    let pos =  bca::search::linear_search([12, 21, 22, 40], 400);
-    bca::search::show_pos_msg(pos);
+    let pos =  algorithms::search::linear_search([12, 21, 22, 40], 400);
+    algorithms::search::show_pos_msg(pos);
 
     println!("Performing binary search");
-    let pos = bca::search::binary_search([10, 12, 15, 16, 17, 18, 19, 20, 21], 120);
-    bca::search::show_pos_msg(pos);
+    let pos = algorithms::search::binary_search([10, 12, 15, 16, 17, 18, 19, 20, 21], 120);
+    algorithms::search::show_pos_msg(pos);
 }
