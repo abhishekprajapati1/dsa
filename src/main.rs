86706a408c49da6253735805a59f13f32f1355c7
abhishekprajapati1 <ap0661236@gmail.com>
2025-07-08 13:57:46 +0530

    nothing special

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-mod algorithms;
 fn main() {
-    println!("Performing linear search.");
-    let pos =  algorithms::search::linear_search([12, 21, 22, 40], 400);
-    algorithms::search::show_pos_msg(pos);
-
-    println!("Performing binary search");
-    let pos = algorithms::search::binary_search([10, 12, 15, 16, 17, 18, 19, 20, 21], 120);
-    algorithms::search::show_pos_msg(pos);
+    // there are two types of strings in rust:
+    // 1. string slices: borrowed, immutable
+    let s1: &str = "abhishek";
+    
+    // to get the length in bytes:
+    let length = s1.
 }
